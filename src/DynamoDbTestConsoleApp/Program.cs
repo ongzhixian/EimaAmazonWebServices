ecc867ddc92dbc253324762d23f68693f9c35831
ongzhixian <zhixian@hotmail.com>
2025-02-09 17:46:07 +0800

    EIMA-50 Add DynamoDbTestConsoleApp

---
+++ src/DynamoDbTestConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Eima.ConfigurationServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var configuration = GetConfiguration();
+
+ServiceProvider serviceProvider = ConfigureServices(configuration);
+
+var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+logger.LogInformation("[START]");
+
+logger.LogInformation("[TEST] {aaa}", configuration["test:sampleSetting1"]);
+
+ConfigurationService configurationService = new ConfigurationService(configuration);
+
+// if (await configurationService.PutConfiguration("TEST", "MyTESTVALUE"))
+// {
+//     logger.LogInformation("ITEM PUT");
+// }
+// else
+// {
+//     logger.LogInformation("ITEM NOT PUT");
+// }
+
+if (await configurationService.PutConfiguration("TEST", "AAA"))
+{
+    logger.LogInformation("ITEM PUT");
+}
+else
+{
+    logger.LogInformation("ITEM NOT PUT");
+}
+
+logger.LogInformation("[END]");
